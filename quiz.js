e99858de951f2dfaca4cde6234f53a1e053ec4ce
arigbede omobolaji <arigbedeomobolaji@gmail.com>
2020-07-14 14:18:02 +0100

    add index.html style.css index.js

---
+++ quiz.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+let question = [
+    {
+        question: "What's my name: ",
+        answer: "Arigbede Omobolaji"
+    },
+    {
+        question: "What's my christian name: ",
+        answer: "paul"
+    },
+    {
+        question: "What's my sister's name : ",
+        answer: ["Arigbede Comfort", "Arigbede Olayinka"]
+    },
+    {
+        question: "Where's my state of origin: ",
+        answer: "Osun State"
+    },
+];
+
+console.log(question[1].answer);
